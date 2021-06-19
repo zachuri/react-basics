b42e5f114285998addc52c29f2a2255d6b913529
zachuri <punsalang.zachary@gmail.com>
2021-06-19 11:45:15 -0700

    learned on how to display array of data by mapping

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -3,54 +3,60 @@ import React from "react";
 export default class FetchRandomUser extends React.Component {
    state = {
       loading: true,
-      person: null,
+      // person: null,
+      people: [],
    };
 
    async componentDidMount() {
-      const url = "https://api.randomuser.me/";
+      const url = "https://api.randomuser.me/?results=5";
 
       // This is asynchronous code
       const response = await fetch(url); // fetch allows us to get the http request from API
       const data = await response.json(); // returns the json data
-      this.setState({ person: data.results[0], loading: false });
-      console.log(data.results[0]);
+      this.setState({ people: data.results, loading: false });
+      // console.log(data.results[0]);
    }
 
    render() {
-      // if (this.state.loading) {
-      //    return <div>loading...</div>;
-      // }
+      if (this.state.loading) {
+         return <div>loading...</div>;
+      }
 
       // if (!this.state.person) {
-      //    return <div>didn't get a person </div>;
-      // }
+      if (!this.state.people) {
+         return <div>didn't get a person </div>;
+      }
 
       return (
-         <div>
-            {this.state.loading || !this.state.person ? (
-               <div>loading...</div>
-            ) : (
-               <div>
-                  <div>{this.state.person.name.title}</div>
-                  <div>{this.state.person.name.first}</div>
-                  <div>{this.state.person.name.last}</div>
-                  <img
-                     src={this.state.person.picture.large}
-                     alt="random user"
-                  />
-               </div>
-            )}
-         </div>
+         // <div>
+         //    {this.state.loading || !this.state.person ? (
+         //       <div>loading...</div>
+         //    ) : (
+         //       <div>
+         //          <div>{this.state.person.name.title}</div>
+         //          <div>{this.state.person.name.first}</div>
+         //          <div>{this.state.person.name.last}</div>
+         //          <img
+         //             src={this.state.person.picture.large}
+         //             alt="random user"
+         //          />
+         //       </div>
+         //    )}
+         // </div>
 
-         /* <div>
-            <div>{this.state.person.name.title}</div>
-            <div>{this.state.person.name.first}</div>
-            <div>{this.state.person.name.last}</div>
-            <img
-               src={this.state.person.picture.large}
-               alt="random user"
-            />
-         </div> */
+         <div>
+            {this.state.people.map((person) => (
+               //Mapping over the datas
+               //Need a unique key
+               //<div key={'some-person-${i}'}> this is a last resort to create an id
+               <div key={person.login.uuid}>
+                  <div>{person.name.title}</div>
+                  <div>{person.name.first}</div>
+                  <div>{person.name.last}</div>
+                  <img src={person.picture.large} alt="random user" />
+               </div>
+            ))}
+         </div>
       );
    }
 }
